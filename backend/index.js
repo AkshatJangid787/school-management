b2c57919a4bc38f6acfac59f890fef52402fbe5d
akshatjangid787 <akshatjangid14@gmail.com>
2025-06-07 13:51:36 +0530

    created controller, model, router for school

--- backend/index.js
+++ backend/index.js
@@ -5,9 +5,26 @@ import mongoose from 'mongoose'
 mongoose.connect(process.env.DB)
 
 .then(()=>console.log("Database connected"))
+
 .catch(()=>console.log("Failed to connect with database"))
 
+import cors from 'cors'
 import express from "express"
-const app = express()
+import SchoolRouter from './router/school.router.js'
+import SubjectRouter from './router/subject.router.js'
+import TeacherRouter from './router/teacher.router.js'
+import ClassRouter from './router/class.router.js'
+import TokenRouter from './router/token.router.js'
 
+const app = express()
 app.listen(8080, ()=>console.log("App is running on 8080"))
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+app.use("/school", SchoolRouter)
+app.use("/subject", SubjectRouter)
+app.use("/teacher", TeacherRouter)
+app.use("/class", ClassRouter)
+app.use("/token", TokenRouter)
